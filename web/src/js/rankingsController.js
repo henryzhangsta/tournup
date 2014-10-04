35a368d5f7780f08c5b4632e219c26868c349902
Apollo Jain <apollojain@gmail.com>
2014-10-04 12:43:41 -0700

    Add RankingsController

---
+++ web/src/js/rankingsController.js
@@ -0,0 +1,19 @@
+var app = angular.module('myApp', ['simplePagination']);
+
+function rankingsController($scope) {
+    $scope.ranks = [
+	 	{name: 'Ryan "420 Blaze It" Hon H$U', 
+		score: 53} ,
+		{name: 'Henry Zhang', 
+		score: 51} , 
+		{name: 'Alex Kang', 
+		score: 12} ,
+		{name: 'Yiwen Zhang', 
+		score: 10} ,
+		{name: 'Ryan Zhang', 
+		score: 8},
+		{name: 'Zhang Huadian', 
+		score: 6}
+	];
+    $scope.orderProp = '-score';
+} 

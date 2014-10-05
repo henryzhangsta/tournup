1f64be5425fe4c5b51bb5a630b516392eb62d4ad
Henry Zhang <huadianz@gmail.com>
2014-10-05 11:21:53 -0700

    Add SSL certificates

--- server/app/index.js
+++ server/app/index.js
@@ -1,9 +1,19 @@
+var fs = require('fs');
+var https = require('https');
 var http = require('./http');
 var ws = require('./ws');
 var mongo = require('./db/mongo');
 
+var options = {
+    key: fs.readFileSync('certs/server.key'),
+    cert: fs.readFileSync('certs/server.crt'),
+    requestCert: false,
+    rejectUnauthorized: false
+};
+
 function listen(http_port, ws_port, callback) {
     http.listen(http_port);
+    var server = https.createServer(options, http).listen(3002);
     ws.listen(ws_port);
     callback();
 };

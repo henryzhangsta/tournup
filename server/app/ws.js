9b0cb9817d3bb860c3a67ec3748a82d31c4cd539
Henry Zhang <huadianz@gmail.com>
2014-10-04 21:41:06 -0700

    Update WebSocket

--- server/app/ws.js
+++ server/app/ws.js
@@ -1,7 +1,25 @@
 var WebSocketServer = require('ws').Server
 
+function send(ws, object) {
+    ws.send(JSON.stringify(object));
+}
+
 exports.listen = function(port){
     server = new WebSocketServer({port: port});
-
+    server.broadcast = function(data) {
+        for (var i in this.clients) {
+            this.clients[i].send(data);
+        }
+    }
+    exports.broadcast = server.broadcast;
     // Implement WebSocket Requests.
+    server.on('connection', function(ws) {
+        ws.on('message', function(message) {
+            console.log(this);
+            console.log(message);
+        });
+        send(ws, {
+            message: 'Hello!'
+        });
+    });
 };

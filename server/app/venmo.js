0b4aa063e41f66539ccc5111b0b1891d5fc45cb6
Henry Zhang <huadianz@gmail.com>
2014-10-04 19:23:34 -0700

    Add Venmo API verification.

--- server/app/venmo.js
+++ server/app/venmo.js
@@ -1,4 +1,10 @@
 
 exports.webhook = function(req, res, next) {
+    if (req.query.venmo_challenge) {
+        res.send(req.query.venmo_challenge);
+        res.end();
+        return;
+    }
+    
     console.log(req.body);
 }
